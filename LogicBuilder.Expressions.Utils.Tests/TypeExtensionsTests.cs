3c922e77c4df5943548ce6dfce95518ea6c53650
Blaise Taylor <btaylor@bPolicySolutions.com>
2022-04-15 09:37:45 -0400

    Minor - getting member infos from declaring type rather than reflected type - GetMemberInfos().

--- LogicBuilder.Expressions.Utils.Tests/TypeExtensionsTests.cs
+++ LogicBuilder.Expressions.Utils.Tests/TypeExtensionsTests.cs
@@ -23,6 +23,19 @@ namespace LogicBuilder.Expressions.Utils.Tests
             Assert.Equal(reflectedType.FullName, memberInfo.ReflectedType.FullName);
         }
 
+        [Theory]
+        [InlineData(nameof(DerivedThing.Id), typeof(DerivedThing))]
+        [InlineData(nameof(DerivedThing.Name), typeof(BaseThing))]
+        [InlineData(nameof(DerivedThing.Description), typeof(DerivedThing))]
+        public void MemberInfoReflectedTypeMustMatchTheDeclaringTypeForGetSelectedMembers(string propertyName, Type reflectedType)
+        {
+            //act
+            MemberInfo memberInfo = typeof(DerivedThing).GetSelectedMembers(new List<string>()).FirstOrDefault(m => m.Name == propertyName);
+
+            //assert
+            Assert.Equal(reflectedType.FullName, memberInfo.ReflectedType.FullName);
+        }
+
         public abstract class BaseThing
         {
             public string Name { get; set; }
