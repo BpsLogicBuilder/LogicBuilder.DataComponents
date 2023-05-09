6df1610be2c6c065b1868ede8f7a65dd4fca40cc
Blaise Taylor <btaylor@bPolicySolutions.com>
2023-05-09 10:56:48 -0400

    Removing deprecated targets and libraries. (#32)
    * Removing deprecated targets and libraries.
    
    * SDK 7.0.203
    
    * net7.0 the tests
    
    * LogicBuilder.Dat must be netstandard.

--- LogicBuilder.Data/BaseData.cs
+++ LogicBuilder.Data/BaseData.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace LogicBuilder.Data
+﻿namespace LogicBuilder.Data
 {
     abstract public class BaseData
     {
-        [NotMapped]
         public EntityStateType EntityState { get; set; }
     }
 }
