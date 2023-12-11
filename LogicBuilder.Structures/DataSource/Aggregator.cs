d8089898c4079d5cf073d773feb08fdd66be48d0
Blaise Taylor <btaylor@bPolicySolutions.com>
2023-12-11 07:33:21 -0500

    Removing the obsolete.

--- LogicBuilder.Structures/DataSource/Aggregator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LogicBuilder.Expressions.Utils.DataSource
-{
-    [System.Obsolete("No longer used. Use LogicBuilder.Expressions.Utils.ExpressionBuilder.")]
-    public class Aggregator
-    {
-        public string Field { get; set; }
-        public string Aggregate { get; set; }
-    }
-}
