7906f87190569e0110e2fdf30b5730dfe04ef558
Blaise Taylor <btaylor@bPolicySolutions.com>
2024-11-02 09:55:16 -0400

    Minor: separation of concerns.

--- LogicBuilder.Expressions.Utils.Tests/TypeExtensionsTests.cs
+++ LogicBuilder.Expressions.Utils.Tests/TypeExtensionsTests.cs
@@ -11,7 +11,6 @@ namespace LogicBuilder.Expressions.Utils.Tests
         [Theory]
         [InlineData(nameof(DerivedThing.Id), typeof(DerivedThing))]
         [InlineData(nameof(DerivedThing.Name), typeof(BaseThing))]
-        [InlineData(nameof(DerivedThing.DataInBytes), typeof(BaseThing))]
         [InlineData(nameof(DerivedThing.Description), typeof(DerivedThing))]
         public void MemberInfoReflectedTypeMustMatchTheDeclaringType(string propertyName, Type reflectedType)
         {
@@ -25,7 +24,6 @@ namespace LogicBuilder.Expressions.Utils.Tests
         [Theory]
         [InlineData(nameof(DerivedThing.Id), typeof(DerivedThing))]
         [InlineData(nameof(DerivedThing.Name), typeof(BaseThing))]
-        [InlineData(nameof(DerivedThing.DataInBytes), typeof(BaseThing))]
         [InlineData(nameof(DerivedThing.Description), typeof(DerivedThing))]
         public void MemberInfoReflectedTypeMustMatchTheDeclaringTypeForGetSelectedMembers(string propertyName,
             Type reflectedType)
@@ -38,47 +36,37 @@ namespace LogicBuilder.Expressions.Utils.Tests
             Assert.Equal(reflectedType.FullName, memberInfo.ReflectedType.FullName);
         }
 
-        [Theory]
-        [InlineData(typeof(DerivedThing))]
-        [InlineData(typeof(BaseThing))]
-        public void GetSelectedMembers_WhenSelectIsEmpty_MustReturnAllLiteralMembers(Type reflectedType)
+        [Fact]
+        public void GetSelectedMembers_WhenSelectIsEmpty_MustReturnAllLiteralAndLiteralListMembers()
         {
             // Act
-            var memberInfos = reflectedType.GetSelectedMembers(Enumerable.Empty<string>().ToList());
+            var memberInfos = typeof(Thing).GetSelectedMembers(Enumerable.Empty<string>().ToList());
 
             // Assert
             Assert.Multiple(() =>
             {
                 var names = memberInfos.Select(mi => mi.Name).ToList();
                 
-                Assert.DoesNotContain(memberInfos, name => name.Name == nameof(BaseThing.Objects));
+                Assert.DoesNotContain(memberInfos, name => name.Name == nameof(Thing.Objects));
                 
-                Assert.Contains(names, name => name == nameof(BaseThing.ParametersArray));
-                Assert.Contains(names, name => name == nameof(BaseThing.ParametersList));
-                Assert.Contains(names, name => name == nameof(BaseThing.Ints));
-                Assert.Contains(names, name => name == nameof(BaseThing.Strings));
-                Assert.Contains(names, name => name == nameof(BaseThing.Booleans));
-                Assert.Contains(names, name => name == nameof(BaseThing.DateTimes));
-                Assert.Contains(names, name => name == nameof(BaseThing.Dates));
-                Assert.Contains(names, name => name == nameof(BaseThing.Guides));
-                Assert.Contains(names, name => name == nameof(BaseThing.UnsignedInts));
+                Assert.Contains(names, name => name == nameof(Thing.ParametersArray));
+                Assert.Contains(names, name => name == nameof(Thing.ParametersList));
+                Assert.Contains(names, name => name == nameof(Thing.Ints));
+                Assert.Contains(names, name => name == nameof(Thing.Strings));
+                Assert.Contains(names, name => name == nameof(Thing.Booleans));
+                Assert.Contains(names, name => name == nameof(Thing.DateTimes));
+                Assert.Contains(names, name => name == nameof(Thing.Dates));
+                Assert.Contains(names, name => name == nameof(Thing.Guides));
+                Assert.Contains(names, name => name == nameof(Thing.UnsignedInts));
+                Assert.Contains(names, name => name == nameof(Thing.Name));
+                Assert.Contains(names, name => name == nameof(Thing.Id));
+                Assert.Contains(names, name => name == nameof(Thing.Description));
             });
         }
 
         private abstract class BaseThing
         {
             public string Name { get; set; }
-            public byte[] DataInBytes { get; set; }
-            public string[] ParametersArray { get; set; }
-            public ICollection<string> Strings { get; set; }
-            public List<string> ParametersList { get; set; }
-            public List<bool> Booleans { get; set; }
-            public ISet<DateTime> DateTimes { get; set; }
-            public ISet<DateOnly> Dates { get; set; }
-            public HashSet<Guid> Guides { get; set; }
-            public uint[] UnsignedInts { get; set; }
-            public IEnumerable<int> Ints { get; set; }
-            public List<object> Objects { get; set; }
         }
 
         private class DerivedThing : BaseThing, IDerivedThing
@@ -90,6 +78,24 @@ namespace LogicBuilder.Expressions.Utils.Tests
         private interface IDerivedThing
         {
             public string Description { get; set; }
+        }
+
+        private class Thing
+        {
+            public string Name { get; set; }
+            public Guid Id { get; set; }
+            public string Description { get; set; }
+            public byte[] DataInBytes { get; set; }
+            public string[] ParametersArray { get; set; }
+            public ICollection<string> Strings { get; set; }
+            public List<string> ParametersList { get; set; }
+            public List<bool> Booleans { get; set; }
+            public ISet<DateTime> DateTimes { get; set; }
+            public ISet<DateOnly> Dates { get; set; }
+            public HashSet<Guid> Guides { get; set; }
+            public uint[] UnsignedInts { get; set; }
+            public IEnumerable<int> Ints { get; set; }
+            public List<object> Objects { get; set; }
         }
     }
 }
